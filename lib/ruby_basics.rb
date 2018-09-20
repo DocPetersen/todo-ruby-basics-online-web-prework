15590eec3dcd89d7dad2b3815a0d119472c6b569
REESE PETERSEN <reese.r.petersen@gmail.com>
2018-09-20 17:39:30 +0000

    automatically backed up by learn

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -14,8 +14,8 @@ def greeting(greeting, name)
   "#{greeting}, #{name}!"
 end
 
-def return_a_value(value = "Nice")
-  "#{value}""
+def return_a_value(value="Nice")
+  "#{value}"
 end
 
 def last_evaluated_value
